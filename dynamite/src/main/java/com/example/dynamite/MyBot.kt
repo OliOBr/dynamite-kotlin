02bd2bec14dac9da94d91ce45ec3f696ffb42a59
OliOB <Oli.OBrien@softwire.com>
2021-07-07 11:00:31 +0100

    Added calculating scores

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -6,14 +6,30 @@ import com.softwire.dynamite.game.Move
 
 class MyBot : Bot {
     var dynamiteSticks = 100
-    var roundNumber = 0
+    var roundNumber = 1
     var myCurrentScore = 0
+    var theirCurrentScore = 0
+    var valueOfRound = 1
+
     override fun makeMove(gamestate: Gamestate): Move {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
-
-        var roundsLeftTillWin =
+        if (roundNumber > 1) {
+            val lastRound = gamestate.rounds.last()
+            val lastRoundOutcome = getOutcome(lastRound.p1,lastRound.p2)
+            when(lastRoundOutcome) {
+                Outcome.WIN -> {myCurrentScore += valueOfRound
+                                valueOfRound = 1}
+                Outcome.LOSS -> {theirCurrentScore += valueOfRound
+                                valueOfRound = 1}
+                Outcome.DRAW -> valueOfRound++
+            }
+        }
+        if(myCurrentScore == 1000-valueOfRound || theirCurrentScore == 1000-valueOfRound || roundNumber == 2500){
+            print("My final score: $myCurrentScore, Their final score: $theirCurrentScore \n")
+        }
         roundNumber ++
+        return Move.S
     }
 
     fun getOutcome(ourMove: Move, theirMove: Move): Outcome{
@@ -53,8 +69,6 @@ class MyBot : Bot {
     }
 
     init {
-
-
         // Are you debugging?
         // Put a breakpoint on the line below to see when we start a new match
         println("Started new match")
